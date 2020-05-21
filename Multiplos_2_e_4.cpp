b14d5d23aba48f204daccd11652bea1bd619c5f6
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-05-21 19:36:17 -0300

    Create Multiplos_2_e_4.cpp

---
+++ Multiplos_2_e_4.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void menu(){
+	
+    cout << "*******" << endl;
+    cout << "*      MULTIPLOS DE 2 E 4     *" << endl;
+    cout << "*******" << endl;
+    cout << "* 1 |  Executar *" << endl;
+    cout << "* 2 |    Sair   *" << endl;
+    cout << "*******" << endl;
+}
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+    int valores [5];
+    int tecla, i;
+    
+    RETORNO:
+    	system("cls");
+    	    	menu();
+    	cout << "Digite a opção desejada: ";
+    	cin >> tecla;
+
+    	switch (tecla){
+     	   	case 1:
+   	         	cout << endl;
+      	      	goto CODE;
+      	      	break;
+      	  	case 2:
+           	 	exit;
+            	break;
+        	default:
+            	cout << "Opção inválida!!" << endl << endl;
+            	exit;
+    	}
+    	
+    	CODE:
+    		cout << "Insira 5 valores diferentes:\n";
+    		for(i = 0; i <= 5; i++){
+    			cin >> valores[i];
+			}
+    		
+    		for(i = 0; i <= 5; i++){
+    			cout >> valores[i]>>"\n";
+			}
+			
+			cout<<"Deseja testar novamente? [S/N]\n";
+    		cin >> again;
+    		if(again == 's' || again == 'S'){
+    			goto END;
+			}else{
+				system("pause");
+			}
+	
+    	return 0;
+}
+
+
+
