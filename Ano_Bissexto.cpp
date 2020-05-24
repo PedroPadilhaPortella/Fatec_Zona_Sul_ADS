e63cbdea7fbddd7a951b46f9d6891685d13ae3d3
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-05-24 00:06:23 -0300

    ano bissexto

---
+++ Ano_Bissexto.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+void menu(){
+    cout << "*******" << endl;
+    cout << "*      MULTIPLOS DE 2 E 4     *" << endl;
+    cout << "*******" << endl;
+    cout << "* 1 |  Executar *" << endl;
+    cout << "* 2 |    Sair   *" << endl;
+    cout << "*******" << endl;
+}
+int main(void){
+	setlocale(LC_ALL, "Portuguese");
+    int tecla, ano;
+    char again;
+    RETORNO:
+    	system("cls");
+    	    	menu();
+    	cout << "Digite a opção desejada: ";
+    	cin >> tecla;
+    	switch (tecla){
+     	   	case 1:
+   	         	cout << endl;
+      	      	goto CODE;
+      	      	break;
+      	  	case 2:
+           	 	goto EXIT;
+            	break;
+        	default:
+            	cout << "Opção inválida!!" << endl << endl;
+            	goto EXIT;
+    	}
+    	CODE:
+    		cout << "Descubra se um ANO e ou nao Bissexto:\n Insira um ano aqui: ";
+    		cin >> ano;
+    		cout << (ano % 4 == 0)? "Este ano e Bissexto!\n" : "Esse ano nao e Bissexto!\n";
+    		
+			cout<<"Deseja testar novamente? [S/N]\n";
+    		cin >> again;
+    		if(again == 's' || again == 'S'){
+    			goto RETORNO;
+			}else{
+				system("pause");
+			}
+		EXIT:
+    		return 0;
+}
