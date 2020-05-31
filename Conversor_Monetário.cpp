c415adbea3736af3cfef9c345bd292bfb13bd8ed
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-05-30 22:25:04 -0300

    Update Conversor_Monetário.cpp

--- Conversor_Monetário.cpp
+++ Conversor_Monetário.cpp
@@ -5,31 +5,29 @@
 
 using namespace std;
 
-double calc(double cotacao, double qtd){
-  double result;
-
-  result = cotacao * qtd;
-
-  return result;
-
-}
-
 void menu(){
 
-    cout << "*****************" << endl;
-    cout << "*      MENU     *" << endl;
-    cout << "*****************" << endl;
-    cout << "* 1 |  Executar *" << endl;
-    cout << "* 2 |    Sair   *" << endl;
-    cout << "*****************" << endl;
+    cout << "********************************" << endl;
+    cout << "*      CONVERSOR MONETARIO     *" << endl;
+    cout << "********************************" << endl;
+    cout << "********** 1 |  Executar *******" << endl;
+    cout << "******** 2 |    Sair   *********" << endl;
+    cout << "********************************" << endl;
 
 }
 
+double ConverterValorMonetario(double cotacaoMoeda, double valorMoeda){
+  double conversaoMoeda;
+  conversaoMoeda = cotacaoMoeda * valorMoeda;
+  return conversaoMoeda;
+}
 
 int main() {
   setlocale(LC_ALL, "Portuguese");
-  double cot, dolar, real;
+  system("cls");
+  double cotacaoBase, valorASerConvertido;
   int tecla;
+  char option [], moeda01 [], moeda02 [];
 
   MENU:
   menu();
@@ -50,15 +48,43 @@ int main() {
   }
 
   A:
-
-  cout << "Digite a cotação do dolar: R$ ";
-  cin >> cot;
-  cout << "Digite o quanto de dólares você possui: $ ";
-  cin >> dolar;
-
-  real = calc(cot, dolar);
-
-  cout << endl << "$" << dolar << " em moeda brasileira vale R$" << real << endl  << endl;
+  	cout << "| DOLAR -> REAL | [DR]\n | EURO -> REAL |[ER]\n| REAL -> DOLAR |[RD]\n| REAL -> EURO |[RE]\n|DOLAR -> EURO |[DE]\n| EURO -> DOLAR |[ED]";
+  	cout << "\n \nConversions Values based on may/30/2020"
+  	cout << "Escolha uma Conversão Monetária: ";
+  	cin >> option;
+  	cout << "Digite o Valor Monetario a Ser Convertida: $ ";
+  	cin >> valorASerConvertido;
+  	
+  	switch (option) {
+  		case 'DR':
+  			cotacaoBase = 0.19;
+  			valorConvertido = ConverterValorMonetario(, valorASerConvertido);
+  			break;
+  		case 'ER':
+  			cotacaoBase = 0.17;
+  			valorConvertido = ConverterValorMonetario(, valorASerConvertido);
+  			break;
+  		case 'RD':
+  			cotacaoBase = 5.33;
+  			valorConvertido = ConverterValorMonetario(, valorASerConvertido);
+  			break;
+		case 'RE':
+			cotacaoBase = 5.92;
+  			valorConvertido = ConverterValorMonetario(, valorASerConvertido);
+  			break;
+		case 'DE':
+			cotacaoBase = 0.9;
+  			valorConvertido = ConverterValorMonetario(, valorASerConvertido);
+  			break;
+		case 'ED':
+			cotacaoBase = 1.11;
+  			valorConvertido = ConverterValorMonetario(, valorASerConvertido);
+  			break;
+  		default:
+  			cout << "Conversão Inváilda, Por favor insira uma conversão Válida!!";
+	  }
+	  
+	  cout << endl << "$" << valorASerConvertido << " em "<< moeda01 <<" vale R$" << valorConvertido << moeda02 << endl  << endl;
 
   goto MENU;
 
