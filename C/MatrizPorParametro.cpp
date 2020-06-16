313a04c8729b18dd34e15deb2bfa9bef92d9265e
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-15 22:05:47 -0300

    Ordenador de Numeros

---
+++ C/MatrizPorParametro.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int matriz [3][3]={1, 2, 3, 4, 5, 6, 7, 8, 9};
+	void functionMatriz(int x [3][3]);
+	functionMatriz(matriz);
+	getchar();
+	return 0; 
+}
+
+void functionMatriz(int x [3][3]){
+	int i, j;
+	for(i = 0; i <= 3; i++){
+		for(j = 0; j <= 3; j++){
+			printf("%i", x[i][j]);
+		}
+	}
+}
