a912e9c4fcd6742eda4861d968c214729939fb5a
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-15 22:25:38 -0300

    matriz Por Parametro

--- C/MatrizPorParametro.cpp
+++ C/MatrizPorParametro.cpp
@@ -10,9 +10,10 @@ int main(){
 
 void functionMatriz(int x [3][3]){
 	int i, j;
-	for(i = 0; i <= 3; i++){
-		for(j = 0; j <= 3; j++){
+	for(i = 0; i < 3; ++i){
+		for(j = 0; j < 3; ++j){
 			printf("%i", x[i][j]);
 		}
+		printf("\n");
 	}
 }
