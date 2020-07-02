8838bf65939a244cfc433758ca11cf2c918e257b
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-02 20:02:59 -0300

    aula 08

---
+++ MaiorMenorSalario.cpp
@@ -0,0 +1,72 @@
+#include "iostream"
+#include "string.h"
+using namespace std;
+
+double cont = 0, salario, salariomaior, salariomenor; 
+double media = 0, saldo = 0;
+
+
+void salarios();
+
+void mostrar();
+
+
+
+int main() 
+{ 
+   setlocale(LC_ALL, "Portuguese");
+
+   int tecla = 0;
+
+    while ( tecla != 3 )
+    { 
+      system("clear");
+      
+      cout << "\n**menu**\n1 Ler\n2 Mostrar\n3 sair\nitem:";
+      cin >> tecla;
+      
+      switch ( tecla )
+      { 
+        case 1: salarios(); 
+                break;
+
+        case 2: mostrar();
+                break;
+
+        case 3: cout << "\nPrograma finalizado!";
+                exit(0); 
+                break;
+      } 
+    }
+             
+}
+
+void salarios ()  
+{
+ cout << "\nSalário " << cont+1 << ":";
+ cin >> salario;  
+ cont ++;  // cont = cont + 1
+
+ saldo += salario; // somatório dos salarios
+ media = saldo / cont;
+ 
+  if ( cont == 1 )
+   { 
+     salariomenor = salario;  
+     salariomaior = salario; 
+    }
+ 
+  if ( salario < salariomenor ) 
+          salariomenor = salario;
+ 
+  if ( salario > salariomaior )  
+          salariomaior = salario;
+}
+
+
+void mostrar() 
+{
+ cout << "\nSalário Menor:" << salariomenor << " | Salário Maior:" << salariomaior << "   Média:" << media << " Saldo :"<< saldo; 
+ system("Sleep 2");
+ 
+}
